48c1ea847e7b96c7bb9a075a402c40861ffd7d82
nigulous <fuckyou@fuckyou.com>
2022-03-11 17:13:13 -0800

    added eslint prettier to the application.

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Our blank native canvas</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBar}>
+        <Text> search</Text>
+      </View>
+      <View style={styles.list}>
+        <Text> list</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-   flex:1,
-   alignItems:"center",
-   justifyContent:"center",
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  searchBar: {
+    flexGrow: 0.04,
+    backgroundColor: "green",
+    width: "auto",
+    justifyContent: "center",
+  },
+  list: {
+    flexGrow: 1,
+    paddingTop: 7,
+    backgroundColor: "blue",
   },
 });
