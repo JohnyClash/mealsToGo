b360e9e32ad4032e891cf2777ce3b2127c745c8a
nigulous <fuckyou@fuckyou.com>
2022-04-03 16:14:01 -0700

    finished with updates(adding mock api call) all is working

--- src/services/restaurants.service.js
+++ src/services/restaurants.service.js
@@ -1,5 +1,28 @@
 
-import { mocks } from './mock/index.js'
+import { mocks } from './mock/index.js';
+import camelize from 'camelize';
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
-    console.log('hello')
+    return new Promise((resolve, reject) => {
+        if (!mocks[location]) {
+            reject('not found');
+        }
+        resolve(mocks[location])
+
+    });
+};
+
+const responseTransformer = ({ results = [] }) => {
+    const mappedResults = results.map((restaurant) => {
+        return {
+            ...restaurant,
+            isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
+            isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+        }
+    })
+    return camelize(mappedResults);
 }
+
+restaurantsRequest().then(responseTransformer)  /// === .then((response)=>responseTranformer(response))
+    .then(transformed => console.log(transformed))
+    .catch((err) => console.log(err))
+
